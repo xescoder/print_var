4ab3130235200abf8534fd8732c4074845cce63e
Alexandr <xescoder@gmail.com>
2013-03-30 13:25:24 +0400

    print_var 0.2 settings

--- print_var 0.2.php
+++ print_var 0.2.php
@@ -1,25 +1,67 @@
 <?php
 
 /**
- * Print variables
- * @param mixed $var Variable
- * @param bool $closed Minimize array and object values
+ * Print variable function
+ * @param mixed $var
  */
-function print_var($var, $closed=false){
-    PrintVarService::Init()->PrintVar($var, $closed);
+function print_var($var){
+    PrintVarService::Init()->PrintVar($var);
 }
 
-class PrintVarService{
+/**
+ * Set settings print_var
+ * @param PrintVarSettings $settings
+ */
+function print_var_settings(PrintVarSettings $settings){
+    PrintVarService::Init($settings);
+}
 
+/**
+ * Settings print_var
+ * Class PrintVarSettings
+ */
+class PrintVarSettings{
+    public $jQuerySource = 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
+    public $jQueryUISource = 'http://code.jquery.com/ui/1.10.2/jquery-ui.js';
+    public $jQueryUIThemeSource = 'http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css';
+
+    public $useJS = true;
+    public $minimize = false;
+
+    public function GetStyle(){
+        return '';
+    }
+
+    public function GetScript(){
+        return '';
+    }
+}
+
+/**
+ * Service print_var
+ * Class PrintVarService
+ */
+class PrintVarService{
     private static $service = null;
 
-    public static function Init(){
-        if(!self::$service) $service = new PrintVarService();
+    public static function Init($settings=null){
+        if(!self::$service || $settings) $service = new PrintVarService($settings);
         return $service;
     }
 
-    private function __construct(){
 
+    private $settings;
+
+    private function __construct($settings){
+        $this->SetSettings($settings);
+    }
+
+    private function SetSettings($settings){
+        if($settings){
+            $this->settings = $settings;
+        } else {
+            $this->settings = new PrintVarSettings();
+        }
     }
 
     public function PrintVar($var, $closed){
